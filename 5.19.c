798948a170887c207c3d57f6ff04893b672a25ea
zhannai <zhangxinpei22@mails.ucas.ac.cn>
2024-04-01 22:27:11 +0800

    4.1更新

---
+++ 5.19.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define INF 0x3f3f3f3f
+#define min(a,b) a<b?a:b
+#define max(a,b) a>b?a:b
+int a[1005][1005];
+int flag[1005][1005];
+int main() {
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int minn,maxn;
+    for(int i=1;i<=n;i++)
+    {
+        minn=INF;
+        for(int j=1;j<=m;j++)
+        {
+            scanf("%d",&a[i][j]);
+            minn=min(minn,a[i][j]);
+        }
+        for(int j=1;j<=m;j++)
+        {
+            if(a[i][j]==minn)flag[i][j]++;
+        }
+    }
+    for(int j=1;j<=m;j++)
+    {
+        maxn=-INF;
+        for(int i=1;i<=n;i++)
+        {
+            maxn=max(maxn,a[i][j]);
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(a[i][j]==maxn)flag[i][j]++;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if(flag[i][j]==2)
+            {
+                printf("%d ",a[i][j]);
+            }
+        }
+    }
+    return 0;
+}
